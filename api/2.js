b5855e3770d751eb017a6ed7c2ce33fa92611bc1
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-25 20:50:20 +0530

    Delete 2.js

--- api/2.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({
-    "name"  : "BT #2",
-    "image" : "https://gateway.btfs.io/btfs/QmaVrs4gG8PgdLnipfzZRZ1CRmbw7RLqeaH83nCzSMgEFJ"
-  })
-}
