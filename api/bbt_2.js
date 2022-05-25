f80fa55a1957582b46f5227b068fb8386374e414
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-25 20:30:10 +0530

    Create bbt_2.js

---
+++ api/bbt_2.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT #2",
+    "image" : "https://gateway.btfs.io/btfs/QmaVrs4gG8PgdLnipfzZRZ1CRmbw7RLqeaH83nCzSMgEFJ"
+  })
+}
