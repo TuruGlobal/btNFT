b1a005681382d960fdbf41d5b3904dc73ec56fc4
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-25 20:51:38 +0530

    Create 2.js

---
+++ api/babyturu/2.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT #1",
+    "image" : "https://gateway.btfs.io/btfs/QmUzFaMYrFbLFaVyekCDjMrH9bP87CxstJf2sFh7zWNva3"
+  })
+}
