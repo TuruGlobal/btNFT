fe896de6a71185273af7d1b1a52fdfae7875ded2
Itsme <54501125+varun13603@users.noreply.github.com>
2022-05-26 14:08:22 +0530

    Create 8.js

---
+++ api/babyturu/8.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT #8",
+    "image" : "https://gateway.btfs.io/btfs/QmVUp9zdXGFZvvwiLPZtXwqEagn8UQAnNWV2SWc1Q4i3YT"
+  })
+}
