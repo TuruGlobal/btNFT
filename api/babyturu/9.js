12a7f7f69cda25947aa1affd618b9437d0c596f3
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-05-26 12:18:38 +0200

    Create 9.js

---
+++ api/babyturu/9.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT #9",
+    "image" : "https://gateway.btfs.io/btfs/QmPq1sWLGV7kntLQ4eb4cUU8LJZSBuBVov4kHGKwfNPodj"
+  })
+}
