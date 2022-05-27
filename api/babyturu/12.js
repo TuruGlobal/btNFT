3ebc78d8c801cf8b31e4aea02b2f8b878fea1af3
TuruGlobal <106305860+TuruGlobal@users.noreply.github.com>
2022-05-27 10:35:53 +0200

    Create 12.js

---
+++ api/babyturu/12.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({
+    "name"  : "BT12 Harley Quinn",
+    "image" : "https://gateway.btfs.io/btfs/QmZUFR4nTCekr1N8SH4BcQbCgFG1ccdWP6qiyqEbmhiPLd"
+  })
+}
